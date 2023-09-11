92219cc00f1cd7c5e02cf8ca92bb6a1cebdf702d
Harold Tan <stan04@syr.edu>
2023-09-11 00:47:07 -0700

    NewsApi Introduction
    Add data model POJO definitions, NewsResponse.
    Add Retrofit interface to support /everything
    and /top-headlines endpoints queries.

---
+++ app/src/main/java/com/example/coffeemeetnews/CoffeeMeetNews.java
@@ -0,0 +1,10 @@
+package com.example.coffeemeetnews;
+
+import android.app.Application;
+
+public class CoffeeMeetNews extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
--- app/src/main/java/com/example/coffeemeetnews/MainActivity.java
+++ app/src/main/java/com/example/coffeemeetnews/MainActivity.java
@@ -6,8 +6,16 @@ import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import com.example.coffeemeetnews.model.NewsResponse;
+import com.example.coffeemeetnews.network.NewsApi;
+import com.example.coffeemeetnews.network.RetrofitClient;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +32,24 @@ public class MainActivity extends AppCompatActivity {
        navController = navHostFragment.getNavController();
        NavigationUI.setupWithNavController(navView, navController);
        NavigationUI.setupActionBarWithNavController(this, navController);
+
+        NewsApi api = RetrofitClient.newInstance().create(NewsApi.class);
+        api.getTopHeadLines("US").enqueue(new Callback<NewsResponse>() {
+            @Override
+            public void onResponse(Call<NewsResponse> call, Response<NewsResponse> response) {
+                if (response.isSuccessful()) {
+                    Log.d("get new top headlines", response.body().toString());
+                }
+                else {
+                    Log.d("getTopHeadlines", response.toString());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<NewsResponse> call, Throwable t) {
+                Log.d("getTopHeadlines", t.getMessage());
+            }
+        });
     }
     @Override
     public boolean onSupportNavigateUp() {
